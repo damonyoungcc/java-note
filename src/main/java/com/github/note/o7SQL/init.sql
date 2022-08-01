8ecaa5c70ce9a4ef4f946de87797a445e803516f
yangdepp <yangdepp@163.com>
2022-08-01 23:58:02 +0800

    fix: fix init sql

--- src/main/java/com/github/note/o7SQL/init.sql
+++ src/main/java/com/github/note/o7SQL/init.sql
@@ -28,25 +28,13 @@ INSERT INTO classes(id, name)
 VALUES (4, '四班');
 
 -- 插入students记录
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (1, 1, '小明', 'M', 90);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (2, 1, '小红', 'F', 95);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (3, 1, '小军', 'M', 88);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (4, 1, '小米', 'F', 73);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (5, 2, '小白', 'F', 81);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (6, 2, '小兵', 'M', 55);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (7, 2, '小林', 'M', 85);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (8, 3, '小新', 'F', 91);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (9, 3, '小王', 'M', 89);
-INSERT INTO students (id, class_id, name, gender, score)
-VALUES (10, 3, '小丽', 'F', 85);
-
-
+INSERT INTO students (class_id, name, gender, score) VALUES (1, '小明', 'M', 90);
+INSERT INTO students (class_id, name, gender, score) VALUES (1, '小红', 'F', 95);
+INSERT INTO students (class_id, name, gender, score) VALUES (1, '小军', 'M', 88);
+INSERT INTO students (class_id, name, gender, score) VALUES (1, '小米', 'F', 73);
+INSERT INTO students (class_id, name, gender, score) VALUES (2, '小白', 'F', 81);
+INSERT INTO students (class_id, name, gender, score) VALUES (2, '小兵', 'M', 55);
+INSERT INTO students (class_id, name, gender, score) VALUES (2, '小林', 'M', 85);
+INSERT INTO students (class_id, name, gender, score) VALUES (3, '小新', 'F', 91);
+INSERT INTO students (class_id, name, gender, score) VALUES (3, '小王', 'M', 89);
+INSERT INTO students (class_id, name, gender, score) VALUES (3, '小丽', 'F', 85);
