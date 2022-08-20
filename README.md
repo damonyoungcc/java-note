f52fc8f52cffd891bb2fc01512f6da3c78504a24
yangdepp <yangdepp@163.com>
2022-08-20 10:43:50 +0800

    feat: mybatis & query cloud mysql

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## java note
 
-docker run --name mysql-java -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7.39
+docker run --restart always --name mysqldb -p 3306:3306 -e MYSQL_ROOT_PASSWORD= -v /mysqldata:/var/lib/mysql -d mysql:5.7.39 --default-time_zone='+8:00'
---
+++ src/main/java/com/github/note/o11Mybatis/Sql.java
@@ -0,0 +1,42 @@
+package com.github.note.o11Mybatis;
+
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Sql {
+    // 用户信息
+    public static class User {
+        Integer id;
+        String name;
+        String tel;
+        String address;
+
+        @Override
+        public String toString() {
+            return "User{" + "id=" + id + ", name='" + name + '\'' + ", tel='" + tel + '\'' + ", address='" + address + '\'' + '}';
+        }
+    }
+
+    interface UserMapper {
+        @Select("select * from user")
+        List<User> getUsers();
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
+        String resource = "db/mybatis/config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            System.out.println(mapper.getUsers());
+        }
+    }
+}
